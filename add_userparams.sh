6f1b3063df870e1431d4f0bbc3a6f8818fa6583c
Guilherme Dill <guilhermealceu@hotmail.com>
2025-06-05 11:41:27 -0300

    Update add_userparams.sh

--- add_userparams.sh
+++ add_userparams.sh
@@ -6,13 +6,14 @@ cat <<EOF >> "$CONF"
 
 UserParameter=temperature.zone0,sysctl -n hw.acpi.thermal.tz0.temperature | sed 's/C//'
 UserParameter=temperature.zone1,sysctl -n hw.acpi.thermal.tz1.temperature | sed 's/C//'
-UserParameter=speedtest.vivo.ping,sh /root/scripts/speedtest_param.sh SUBSTITUA-IP-DO-GETWAY ping
-UserParameter=speedtest.vivo.download,sh /root/scripts/speedtest_param.sh SUBSTITUA-IP-DO-GETWAY download
-UserParameter=speedtest.vivo.upload,sh /root/scripts/speedtest_param.sh SUBSTITUA-IP-DO-GETWAY upload
-UserParameter=speedtest.ligga.ping,sh /root/scripts/speedtest_param.sh SUBSTITUA-IP-DO-GETWAY ping
-UserParameter=speedtest.ligga.download,sh /root/scripts/speedtest_param.sh SUBSTITUA-IP-DO-GETWAY download
-UserParameter=speedtest.ligga.upload,sh /root/scripts/speedtest_param.sh SUBSTITUA-IP-DO-GETWAY upload
-UserParameter=pfSense.lastlogin,/root/scripts/last_login.sh
+
+UserParameter=speedtest.vivo.ping,sh /root/scripts/speedtest_csv_param.sh Vivo ping
+UserParameter=speedtest.vivo.download,sh /root/scripts/speedtest_csv_param.sh Vivo download
+UserParameter=speedtest.vivo.upload,sh /root/scripts/speedtest_csv_param.sh Vivo upload
+
+UserParameter=speedtest.ligga.ping,sh /root/scripts/speedtest_csv_param.sh Ligga ping
+UserParameter=speedtest.ligga.download,sh /root/scripts/speedtest_csv_param.sh Ligga download
+UserParameter=speedtest.ligga.upload,sh /root/scripts/speedtest_csv_param.sh Ligga upload
 EOF
 
-echo "Parâmetros adicionados com sucesso! Lembre-se de substituir ip do getway e outras coisas se precisar!"
+echo "Parâmetros adicionados com sucesso!"
